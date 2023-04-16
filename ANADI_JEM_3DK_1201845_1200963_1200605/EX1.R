7dd9b50ecb9a8ff7c027b84be865e99f76a85174
Marta Portugal <1201845@isep.ipp.pt>
2023-04-17 00:04:46 +0100

    no message

--- ANADI_JEM_3DK_1201845_1200963_1200605/EX1.R
+++ ANADI_JEM_3DK_1201845_1200963_1200605/EX1.R
@@ -117,61 +117,49 @@ cores[mes_max] <- "#B2D3C2"
 barplot(dados_mensais_bomba1$`Média de barris produzidos`, col = cores, names.arg = dados_mensais_bomba1$Mês, xlab = "Meses", ylab = "Média da produção de barris de óleo", main = "Produção de barris de óleo entre Junho de 2013 e Maio de 2014 da Bomba 1")
 
 
-
 #iii)
-#Cálculo das médias da produção diária de barris de óleo para as bombas 1 e 2
+# Cálculo das médias da produção diária de barris de óleo para as bombas 1 e 2
 dados_diarios_bomba1 <- aggregate(as.numeric(barris$OilRateICO1.8) ~ format(as.Date(barris$TimeFormatted), "%Y-%m-%d"), data = barris, FUN = mean)
 dados_diarios_bomba2 <- aggregate(as.numeric(barris$OilRateICO2.8) ~ format(as.Date(barris$TimeFormatted), "%Y-%m-%d"), data = barris, FUN = mean)
 
-#Gerar 10 dias aleatórios, sendo que os dias foram numerados por ordem crescente, ou seja, 1 representa o dia 1 de junho de 2013, 2 representa o dia 2 de junho de 2013,... etc
+# Gerar 10 dias aleatórios, sendo que os dias foram numerados por ordem crescente, ou seja, 1 representa o dia 1 de junho de 2013, 2 representa o dia 2 de junho de 2013,... etc
 set.seed(300)
 dias_aleatorios <- sample(1:365, 10)
 
-#Converter os índices aleatórios nas datas correspondentes
+# Converter os índices aleatórios nas datas correspondentes
 datas_iniciais <- as.Date("2013-06-01")
 datas_aleatorias <- datas_iniciais + as.difftime(dias_aleatorios - 1, units = "days")
 
-#Converter as datas em formato de texto para correspondência com os dados agregados
+# Converter as datas em formato de texto para correspondência com os dados agregados
 datas_aleatorias_formatadas <- format(datas_aleatorias, "%Y-%m-%d")
 
-#Filtrar as médias calculados pelos dias aleatoriamente gerados
+# Filtrar as médias calculadas pelos dias aleatoriamente gerados
 medias_aleatorias_bomba1 <- dados_diarios_bomba1[dados_diarios_bomba1$`format(as.Date(barris$TimeFormatted), "%Y-%m-%d")` %in% datas_aleatorias_formatadas, ]
 medias_aleatorias_bomba2 <- dados_diarios_bomba2[dados_diarios_bomba2$`format(as.Date(barris$TimeFormatted), "%Y-%m-%d")` %in% datas_aleatorias_formatadas, ]
 
-
-#Junção das duas. tabeles com a média
+# Junção das duas tabelas com a média
 medias_aleatorias <- cbind(medias_aleatorias_bomba1, medias_aleatorias_bomba2)
 
-#Edição do nome das colunas
+# Edição do nome das colunas
 colnames(medias_aleatorias) <- c("Dia", "Média Bomba 1","Dia", "Média Bomba 2" )
 
-
-#Criar boxplot para a bomba 1 e bomba 2
+# Criar boxplot para a bomba 1 e bomba 2
 box <- boxplot(medias_aleatorias$`Média Bomba 1`, medias_aleatorias$`Média Bomba 2`, 
                names = c("Bomba 1", "Bomba 2"),
                main = "Produção Diária - Bomba 1 vs. Bomba 2",
                ylab = "Produção Diária (barris)",
                col = c("lightblue", "pink"))
 
-
 print(box)
 
 
 
 #iv
-# Definir a semente para gerar números aleatórios
-set.seed(300)
-
-# Amostra aleatória de 10 dias
-amostra_dias <- sample(1:365, 10)
-
-# Extrair os dados de produção diária para a amostra de dias na bomba 1 e bomba 2
-producao_bomba1 <- as.numeric(dados$OilRateICO1.8[amostra_dias])
-producao_bomba2 <- as.numeric(dados$OilRateICO2.8[amostra_dias])
-
-# Realizar o teste t pareado
-resultado_teste <- t.test(producao_bomba1, producao_bomba2, paired = TRUE)
+#Calcular as estatísticas de teste
+t_test <- t.test(medias_aleatorias$`Média Bomba 1`, medias_aleatorias$`Média Bomba 2`,
+                 alternative = "greater", var.equal = FALSE)
 
 # Exibir os resultados do teste
-print(resultado_teste)
+print(t_test)
+
 
